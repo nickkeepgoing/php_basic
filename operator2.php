429e82568cbe8740e600246f78ca7c0cce3431d6
Nick <nick01.nick02.nick03@gmail.com>
2026-05-27 20:53:17 +0700

    add operator1.php operator2.php superglobal.php

---
+++ operator2.php
@@ -0,0 +1,10 @@
+<?php
+$a =500;
+$b =500;
+
+// var_dump($a == $b);
+// var_dump($a === $b);
+var_dump($a !== $b);
+//var_dump($a != $b);
+
+?>
---
+++ superglobal.php
@@ -0,0 +1,24 @@
+<?php
+/* 
+ ## Superglobal variable.
+ 
+ $GLOBALS - for everyone can use.
+
+ $_SERVER - save webserver config.
+
+ $_GET - collect array form url.
+
+ $_POST - collect data form post method.
+
+ $ENV - environment variable server.
+
+ $_SESSION - save session variable.
+ 
+ $_COOKIE - save cookie variable.
+
+*/
+
+
+
+
+?>
